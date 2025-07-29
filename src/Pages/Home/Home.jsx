6521416b21b7656e8cd0709f8bec556f602c8d0f
Makvana Jayraj <makvana.jayraj76537@gmail.com>
2025-07-29 16:10:41 +0530

    React Project YouTube-Clone

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import './Home.css'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import Feed from '../../Components/Feed/Feed'
+const Home = ({sidebar}) => {
+  const [category,setCategory] = useState(0);
+  return (
+    <>
+      <Sidebar sidebar={sidebar} category={category} setCategory={setCategory} />
+      <dir className={`container ${sidebar?"":"large-container"}`}>
+        <Feed category={category}/>
+      </dir>
+    </>
+  )
+}
+
+export default Home
